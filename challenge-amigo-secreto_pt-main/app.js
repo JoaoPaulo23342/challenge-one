6ab8156e127e210dd5bb3dbcbac23acc7d6f6bea
João <Joaopauloresendecosta55@gmail.com>
2025-01-19 15:21:24 -0300

    finalizando o projeto

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -2,9 +2,15 @@ let nomes = []
 
 const inputNome = document.querySelector('#amigo')
 const paragrafo = document.querySelector('#paragrafo')
+const resultado = document.querySelector('#resultado')
 function adicionar() {
     
     let valor = inputNome.value
+    if(valor === '') {
+        alert('Digite um nome valido')
+        return;
+    }
+
     nomes.push(valor)
 
     inputNome.value = ''
@@ -15,6 +21,18 @@ function adicionar() {
 }
 
 function adicionarAmigo() {
+    
     adicionar()
     paragrafo.innerHTML = nomes.join('<br>')
+}
+
+function sortearAmigo() {
+    if (nomes.length === 0) {
+        resultado.innerHTML = 'Nenhum amigo foi adicionado ainda!';
+        return; 
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * nomes.length);
+    let nomeSorteado = nomes[indiceAleatorio]; 
+    resultado.innerHTML = `O nome sorteado foi: ${nomeSorteado}`; 
 }
